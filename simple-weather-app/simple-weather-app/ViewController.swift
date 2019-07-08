abea6219706b07f3ff468c9ef82247270b154cc9
Adrian Kasiczak <adrian.kasiczak@agora.pl>
2019-07-08 19:46:08 +0200

    Iniitial Commit

---
+++ simple-weather-app/simple-weather-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-weather-app
+//
+//  Created by Adrian Kasiczak on 08/07/2019.
+//  Copyright © 2019 Adrian Kasiczak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
